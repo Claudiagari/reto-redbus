550d9965221642dc17a5495a52fd66376f29e2df
Claudiagari <claudia.garibotto@gmail.com>
2018-03-17 16:37:49 -0500

    añadiendo inicio de reto

---
+++ src/lib/components/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+const App = () => (
+  <div>
+    <div className="col-2">
+      <img className="img-fluid" src="pagoefectivo.png" alt="" />
+    </div>
+    <ul className="header">
+      <li><a href="/">Banca por internet</a></li>
+      <li><a href="/stuff">Pagar en efectivo</a></li>
+    </ul>
+    <div className="content">
+    </div>
+  </div>
+);
+export default App;
