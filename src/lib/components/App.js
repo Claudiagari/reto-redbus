6e4bcc7f45722fbb93391b360587cf914f7d3be8
LidiaRamirez <lid_26_90@hotmail.com>
2018-03-17 20:27:14 -0500

    Routing

--- src/lib/components/App.js
+++ src/lib/components/App.js
@@ -1,15 +1,31 @@
 import React from "react";
+
+import {
+    Route,
+    NavLink,
+    HashRouter
+  } from "react-router-dom";
+import Internet from './internet';
+import Efectivo from './efectivo';
+
 const App = () => (
+  <HashRouter>
   <div>
     <div className="col-2">
       <img className="img-fluid" src="pagoefectivo.png" alt="" />
     </div>
     <ul className="header">
-      <li><a href="/">Banca por internet</a></li>
-      <li><a href="/stuff">Pagar en efectivo</a></li>
+      <li><NavLink to="/" className="active">Banca por internet</NavLink></li>
+      <li><NavLink to="/efectivo" className="active">Pagar en efectivo</NavLink></li>
+      
     </ul>
-    <div className="content">
+    <div className="content"> 
+    <Route exact path="/" component={Internet} />
+    <Route path="/efectivo" component={Efectivo} />
+     
     </div>
   </div>
+  </HashRouter>
 );
+
 export default App;
---
+++ src/lib/components/efectivo.js
@@ -0,0 +1,7 @@
+import React from 'react' ;
+
+const Efectivo = () => (
+  <h1>Efectivo</h1>
+)
+
+export default Efectivo
