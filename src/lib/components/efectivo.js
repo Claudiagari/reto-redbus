417ff18fe0a85cb72b0defe88e00978cde103517
Claudiagari <claudia.garibotto@gmail.com>
2018-03-19 00:45:45 -0500

    añadiendo avance footer

--- src/lib/components/efectivo.js
+++ src/lib/components/efectivo.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import InfoCompra from './info-compra';
 import BtnEfectivo from './btn-efectivo'
+import Footer from './footer';
 const Efectivo = () => (
   <div>
     <InfoCompra />
     <BtnEfectivo />
+    <Footer/>
   </div>
 )
 
---
+++ src/lib/components/footer.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import '../style/footer.css'
+const Footer = () => (
+<div className="container">
+<div className="row">
+<div className="col-4">
+<p>Instrucciones vía <span><i className="fas fa-print"></i></span></p>
+</div>
+</div>
+</div>
+)
+export default Footer
