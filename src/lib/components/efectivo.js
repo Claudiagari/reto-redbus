ebfede60d030abf1a7959e088f84db2da7f16da5
Claudiagari <claudia.garibotto@gmail.com>
2018-03-19 20:48:17 -0500

    footer terminado

--- src/lib/components/efectivo.js
+++ src/lib/components/efectivo.js
@@ -3,12 +3,13 @@ import React from 'react';
 import InfoCompra from './info-compra';
 import SectionPayCash from './SectionPayCash';
 import BtnEfectivo from './btn-efectivo';
+import Footer from './footer';
 const Efectivo = () => (
   <div>
    <InfoCompra />
    <SectionPayCash />
    <BtnEfectivo />
-   
+   <Footer />
   </div>
 )
 
