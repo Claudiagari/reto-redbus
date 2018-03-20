3d0a416cbeaa5d6813d1c736c930c5a7dfa1f9ea
LidiaRamirez <lid_26_90@hotmail.com>
2018-03-20 15:42:38 -0500

    arreglos en slider

--- src/lib/components/Instructions.js
+++ src/lib/components/Instructions.js
@@ -4,6 +4,7 @@ const Instructions = () => (
   <div className="instructions">
     <h6>Instrucciones de pago</h6>
     <p>Selecciona la opción Pago de servicios > De Instituciones y Empresas > Busca por nombre > PAGOEFECTIVO > PAGOEFECTIVOSOLES.</p>
+    <p>Agentes BCP: Brinda el código de empresa 02186</p>
   </div>
 )
 export default Instructions
---
+++ src/lib/store.js
@@ -0,0 +1,40 @@
+import {createStore} from 'redux';
+const initialState= [
+    {
+      id: 'bcp',
+      url: 'bcp.png',
+      instructions1: 'Selecciona la opción Pago de servicios > EMPRESAS > PAGOEFECTIVO > PAGOEFECTIVOSOLES.',
+      instructions2: 'Ingresa tu código CIP: 9125682 y sigue los pasos'
+    },
+    {
+      id: 'bbva',
+      url: 'bbva.png',
+      instructions1: 'Selecciona la opción Pago de servicios > De Instituciones y Empresas > Busca por nombre > PAGOEFECTIVO > PAGOEFECTIVOSOLES.',
+      instructions2: 'Ingresa tu código CIP: 9125682 y sigue los pasos'
+    },
+    {
+      id: 'interbank',
+      url: 'interbank.png',
+      instructions1: 'Selecciona la opción Pago a Instituciones o Empresa > Empresa: PAGOEFECTIVO > Servicio: PAGOEFECTIVO',
+      instructions2: 'Ingresa tu código CIP: 9125682 y sigue los pasos'
+    },
+    {
+      id: 'scotiabank',
+      url: 'scotiabank.png',
+      instructions1: 'Selecciona la opción Pagos > Otras Instituciones > Otros > Busca por Empresa/Servicio: PAGOEFECTIVO > Selecciona: PAGOEFECTIVO SOLES',
+      instructions2: 'Ingresa tu código CIP: 9125682 y sigue los pasos'
+    },
+    {
+      id: 'banbif',
+      url: 'banbif.png',
+      instructions1: 'Selecciona la opción Pago de servicio',
+      instructions2: 'Ingresa tu código CIP: 9125682 y sigue los pasos'
+    }
+  ];
+
+
+const store = createStore(
+  (state) => state,
+  initialState,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+)
