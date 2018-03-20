031df18f793b66dc41913b9a8b8087d896e3e4e2
LidiaRamirez <lid_26_90@hotmail.com>
2018-03-20 18:42:00 -0500

    Cambio de rutas de imagenes

--- src/lib/reducers/dataOnline.js
+++ src/lib/reducers/dataOnline.js
@@ -1,32 +1,38 @@
+import bcp from '../assets/images/bcp.png';
+import bbva from '../assets/images/bbva.png';
+import interbank from '../assets/images/interbank2.jpg';
+import scotiabank from '../assets/images/scotiabank.gif';
+import banbif from '../assets/images/banbif.png';
+
 export default function () {
     return [
         {
           name: 'bcp',
-          url: 'bcp.png',
+          url: bcp,
           instructions1: 'Selecciona la opción Pago de servicios > EMPRESAS > PAGOEFECTIVO > PAGOEFECTIVOSOLES.',
           instructions2: 'Ingresa tu código CIP: 9125682 y sigue los pasos'
         },
         {
           name: 'bbva',
-          url: 'bbva.png',
+          url: bbva,
           instructions1: 'Selecciona la opción Pago de servicios > De Instituciones y Empresas > Busca por nombre > PAGOEFECTIVO > PAGOEFECTIVOSOLES.',
           instructions2: 'Ingresa tu código CIP: 9125682 y sigue los pasos'
         },
         {
           name: 'interbank',
-          url: 'interbank.png',
+          url: interbank,
           instructions1: 'Selecciona la opción Pago a Instituciones o Empresa > Empresa: PAGOEFECTIVO > Servicio: PAGOEFECTIVO',
           instructions2: 'Ingresa tu código CIP: 9125682 y sigue los pasos'
         },
         {
           id: 'scotiabank',
-          url: 'scotiabank.png',
+          url: scotiabank,
           instructions1: 'Selecciona la opción Pagos > Otras Instituciones > Otros > Busca por Empresa/Servicio: PAGOEFECTIVO > Selecciona: PAGOEFECTIVO SOLES',
           instructions2: 'Ingresa tu código CIP: 9125682 y sigue los pasos'
         },
         {
           id: 'banbif',
-          url: 'banbif.png',
+          url: banbif.png,
           instructions1: 'Selecciona la opción Pago de servicio',
           instructions2: 'Ingresa tu código CIP: 9125682 y sigue los pasos'
         }
