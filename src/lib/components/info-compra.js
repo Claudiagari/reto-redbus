c0a54a60011cf4c741196147ff6d3633807381a2
Claudiagari <claudia.garibotto@gmail.com>
2018-03-21 13:33:34 -0500

    añadiendo estilos movile

--- src/lib/components/info-compra.js
+++ src/lib/components/info-compra.js
@@ -6,22 +6,22 @@ import '../assets/icons/style.css'
 const InfoCompra =() => (
   <div className="container">
     <div className="row info-compra">
-      <div className="col-4 text-center info codigo">
+      <div className="col-5 col-md-4 text-center info codigo">
         <p>Código de pago</p>
         <h3><strong>9125682</strong></h3>
       </div>
-      <div className="col-4 text-center info total">
+      <div className="col-2 col-md-4 text-center info total">
         <h4><strong>Total</strong></h4>
         <h4>S/.140.00</h4>
       </div>  
-      <div className="col-4 text-center info fecha align-middle">
+      <div className="col-5 col-md-4 text-center info fecha align-middle">
         <div className="row">
-          <div className="col-4 align-middle">
+          <div className=" col-3 col-md-4 align-middle">
           <span className="icon-alarmclock"></span>
           </div>
-          <div className=" col-8 align-middle">
-            <p>Tu orden expirará :</p>
-            <h5>23 Feb - 01:12 p.m.</h5>
+          <div className="col-9 col-md-8 align-middle expire">
+            <p>Tu orden expirará : <br/><strong>23 Feb - 01:12 p.m.</strong></p>
+           
           </div>
         </div>
       </div>
