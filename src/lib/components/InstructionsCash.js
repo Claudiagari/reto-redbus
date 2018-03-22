b5154de82c8ab8489ceb70caf4c23f19645604be
Claudiagari <claudia.garibotto@gmail.com>
2018-03-22 03:04:47 -0500

    añadiendo

--- src/lib/components/InstructionsCash.js
+++ src/lib/components/InstructionsCash.js
@@ -5,8 +5,8 @@ const InstructionsCash = ({dataInstructionsCash}) => (
     <h6>Instrucciones de pago</h6>
     <p>{dataInstructionsCash.instructions1}</p>
     <p>{dataInstructionsCash.instructions2}</p>
-    <p className="instruction3"><span style={{...(dataInstructionsCash.instructions3 == '' ? {display:'none'} : {color:'#e0aa00', fontSize: '12px', fontWeight: 'bold'} )}}>(*)</span> {dataInstructionsCash.instructions3}</p>
-    <p className="instruction3"><span style={{...(dataInstructionsCash.instructions4 == '' ? {display:'none'} : {color:'#e0aa00', fontSize: '12px', fontWeight: 'bold'} )}}>(*)</span> {dataInstructionsCash.instructions4}</p>
+    <p className="instruction3 "><span style={{...(dataInstructionsCash.instructions3 == '' ? {display:'none'} : {color:'#e0aa00', fontSize: '12px', fontWeight: 'bold'} )}}>(*)</span> {dataInstructionsCash.instructions3}</p>
+    <p className="instruction3 right"><span style={{...(dataInstructionsCash.instructions4 == '' ? {display:'none'} : {color:'#e0aa00', fontSize: '12px', fontWeight: 'bold'} )}}>(*)</span> {dataInstructionsCash.instructions4}</p>
   </div>
 )
 
