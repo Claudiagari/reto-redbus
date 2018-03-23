83086a7fc4301605ec3a08126f43ff86431ea53d
Claudiagari <claudia.garibotto@gmail.com>
2018-03-22 23:15:21 -0500

    añadiendo cambios

--- src/lib/components/App.js
+++ src/lib/components/App.js
@@ -6,24 +6,22 @@ import logo from '../assets/images/pagoefectivo.png';
 
 const App = () => (
   <Router>
-  
-  <div className="container">
+    <div className="container">
     <div className="row">
     <div className="col-6 col-md-2">
       <img className="img-fluid" src={logo} alt="logo" />
     </div>
     </div>
     <ul className="header ">
-      <li><NavLink  exact className= "title" activeStyle={{fontWeight: 'bold', color: '#e0aa00',border:'1px solid #e0aa00', borderRadius:'5px', borderBottom:'0px'}} to="/" >Banca por internet</NavLink></li>
+      <li><NavLink  exact className="title" activeStyle={{fontWeight: 'bold', color: '#e0aa00',border:'1px solid #e0aa00', borderRadius:'5px', borderBottom:'0px'}} to="/reto-redbus" >Banca por internet</NavLink></li>
       <li><NavLink className= "title" activeStyle={{fontWeight: 'bold',color: '#e0aa00',border:'1px solid #e0aa00', borderRadius:'5px', borderBottom:'0px'}} to="/efectivo" >Pagar en efectivo</NavLink></li>
     </ul>
     <div className="content"> 
-    <Route exact path="/" component={Internet} />
+    <Route exact path="/reto-redbus" component={Internet} />
     <Route path="/efectivo" component={Efectivo} />
-      </div>
-  </div>
+    </div>
+    </div>
   </Router>
-  
 );
 
 export default App;
