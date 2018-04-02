ffc96af191ecf86497d5bf470cae9d99a47c03af
claudiagari <claudia.garibotto@gmail.com>
2018-04-02 01:37:01 -0500

    juntando cash y online

--- src-eng/lib/reducers/index.js
+++ src-eng/lib/reducers/index.js
@@ -1,12 +1,16 @@
 import { combineReducers } from 'redux';
 import dataOnline from './dataOnline';
 import instructionDetail from './instructionDetail';
+import dataCash from './dataCash';
+import instructionDetailCash from './instructionsDetailCash'
 
 
 
 const rootReducer = combineReducers({
   active_data: instructionDetail,
-  data: dataOnline
+  data: dataOnline,
+  active_dataCash: instructionDetailCash,
+  dataCash: dataCash,
  }
 )
 
