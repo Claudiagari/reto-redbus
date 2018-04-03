f20ee4fff0e4497a406023a125597344ca422507
LidiaRamirez <lid_26_90@hotmail.com>
2018-04-02 21:42:58 -0500

    Cambio

--- src/lib/components/SelectLanguaje.js
+++ src/lib/components/SelectLanguaje.js
@@ -2,7 +2,7 @@ import React from 'react';
 import '../style/select-languaje.css'
 
 const SelectLanguaje = () => (  
-  <ul className=" col-6 col-md-8 selectLanguaje">
+  <ul className=" col-6 col-md-10 selectLanguaje">
     <li><a href="https://claudiagari.github.io/reto-redbus">ESPAÑOL</a></li>
     <li>|</li>
     <li><a href="https://claudiagari.github.io/reto-redbus-en">ENGLISH</a></li>
